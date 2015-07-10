d1c9dadf32293b19ab6a03fd86ba526b8df3e3f8
Wilfred Wee <weezhizhiang@hotmail.com>
2015-07-10 14:42:57 -0700

    Initial Django skeleton.

---
+++ ds_medical_care/ds_medical_care/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ds_medical_care project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ds_medical_care.settings")
+
+application = get_wsgi_application()
