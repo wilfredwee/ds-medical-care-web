58d74e4feea43a46322876d87023801b7c337a5a
adrianlim <lim-adrian@hotmail.com>
2015-07-21 21:57:20 -0700

    Expanded Child Model to include more fields and created Sleep entry Model. These are placeholders for now and could change later on.
    Models are unverified but look fine to me... I'm missing what looks to be a config file.

--- ds_medical_care/ds_medical_care_web/models.py
+++ ds_medical_care/ds_medical_care_web/models.py
@@ -3,17 +3,25 @@ from django.db.models.signals import post_save
 from django.db import models
 from django.contrib.auth.models import User
 
+class SleepEntry(models.Model):
+    child = models.ForeignKey('Child')
+    timeEvent = models.DateTimeField()
+    timeEntered = models.DateTimeField()
+    notes = models.TextField(blank=True)
 
 class Child(models.Model):
+    # Django creates id field automatically if no primary key specified,
+    # May be useful to have this declared explicitly later on in development
+    id = models.AutoField(primary_key=True)
     parent = models.ForeignKey('ParentProfile', related_name='children')
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200, blank=True)
+    last_name = models.CharField(max_length=200, blank=True)
+    age = models.IntegerField(blank=True)
+    date_of_birth = models.DateField(blank=True)
 
     def __unicode__(self):
         return self.first_name + " " + self.last_name
 
-
-# TODO: Add relationship to Child
 class ParentProfile(models.Model):
     user = models.OneToOneField(User)
     # NOTE: This is temporary, it's just to show what's possible.
